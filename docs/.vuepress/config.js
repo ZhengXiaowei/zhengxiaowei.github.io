c7d7187d2d782b8a2ddcc0706f53f40cdc6c9c7d
xiao <503325017@qq.com>
2018-07-06 10:18:28 +0800

    logo 离线缓存

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,12 +3,25 @@ module.exports = {
   description: 'Blog for Xiao, to record my exp',
   head: [
     ['link', { rel: 'icon', href: '/logo.png' }],
+    ['link', {rel: 'manifest', href: '/manifest.json'}],
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     [
       'meta',
       { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }
-    ]
+    ],
+    [
+      'link',
+      { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }
+    ],
+    [
+      'meta',
+      {
+        name: 'msapplication-TileImage',
+        content: '/icons/msapplication-icon-144x144.png'
+      }
+    ],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
   ],
   themeConfig: {
     sidebar: [
